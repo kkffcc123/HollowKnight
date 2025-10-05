747ae50cbd1518c179a5f56179285f32a3a7bc55
kkffcc123 <2108224724@qq.com>
2025-10-05 16:11:55 +0800

    2025.10.5.1

--- hollowknight/player/player.gd
+++ hollowknight/player/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 var gravity = 300
 var horizontal_move_speed = 150
 var horizontal_move_direciton : Vector2 = Vector2.ZERO
 var deceleraion_speed = 30
+
 var jump_height = 200
 var lower_jump = 4
+var jump_state
 
 func _ready() -> void:
 	pass
@@ -14,6 +19,7 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	horizontal_move()
 	jump_logic(delta)
+	set_animation()
 	move_and_slide()
 	
 func horizontal_move() -> void:
@@ -24,11 +30,31 @@ func horizontal_move() -> void:
 		velocity.x = move_toward(velocity.x,0,deceleraion_speed)
 	
 func jump_logic(delta : float) -> void:
-	var jump_state
 	if Input.is_action_just_pressed("jump"):
 		jump_state = -1
-	
+																																																																																																																																																							   
 	if is_on_floor() and jump_state == -1:
 		velocity.y = jump_state * jump_height
 	if velocity.y < 0 and not Input.is_action_pressed("jump"):
 		velocity.y += gravity * delta * lower_jump
+		jump_state = 0
+
+func set_sprite_flip() -> void:
+	if horizontal_move_direciton.x == -1:
+		sprite_2d.flip_h = true
+	elif horizontal_move_direciton.x == 1:
+		sprite_2d.flip_h = false
+
+func set_animation() -> void:
+	set_sprite_flip()
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			animation_player.play("jump")
+		if velocity.y > 0:
+			animation_player.play("fall")
+		
+	elif horizontal_move_direciton.x:
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
